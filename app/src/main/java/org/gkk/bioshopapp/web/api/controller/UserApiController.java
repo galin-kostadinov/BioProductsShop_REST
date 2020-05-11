e085c9d024fab395d375ea4e2cdf0f9a279485f1
galin-kostadinov <gk_1988@abv.bg>
2020-05-11 14:48:52 +0300

    added getProfile()

--- app/src/main/java/org/gkk/bioshopapp/web/api/controller/UserApiController.java
+++ app/src/main/java/org/gkk/bioshopapp/web/api/controller/UserApiController.java
@@ -1,6 +1,8 @@
 package org.gkk.bioshopapp.web.api.controller;
 
+import org.gkk.bioshopapp.service.model.user.UserProfileServiceModel;
 import org.gkk.bioshopapp.service.service.UserService;
+import org.gkk.bioshopapp.web.api.model.UserProfileResponseModel;
 import org.gkk.bioshopapp.web.view.model.user.UserProfileViewModel;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,11 +30,20 @@ public class UserApiController {
     }
 
     @GetMapping(value = "/all-users")
-    public ResponseEntity<List<UserProfileViewModel>> getAllUsers() {
-        List<UserProfileViewModel> users = this.userService.getAllUsers().stream()
-                .map(userService -> this.modelMapper.map(userService, UserProfileViewModel.class))
+    public ResponseEntity<List<UserProfileResponseModel>> getAllUsers() {
+        List<UserProfileResponseModel> users = this.userService.getAllUsers().stream()
+                .map(userService -> this.modelMapper.map(userService, UserProfileResponseModel.class))
                 .collect(Collectors.toList());
 
         return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/profile")
+    public ResponseEntity<UserProfileResponseModel> getProfile(HttpSession session) throws Exception {
+        String username = session.getAttribute("username").toString();
+        UserProfileServiceModel serviceModel = this.userService.getUserByUsername(username);
+        UserProfileResponseModel user = this.modelMapper.map(serviceModel, UserProfileResponseModel.class);
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
