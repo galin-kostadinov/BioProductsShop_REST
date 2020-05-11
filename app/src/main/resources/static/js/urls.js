082aa44377cfc09e72ae9d2ec3c79a6df879c868
galin-kostadinov <gk_1988@abv.bg>
2020-05-11 13:54:21 +0300

    added urls

--- app/src/main/resources/static/js/urls.js
+++ app/src/main/resources/static/js/urls.js
@@ -1,3 +1,7 @@
 const URLS = {
+    allUsers: '/api/user/all-users',
+    orders: '/api/order/orders',
+    products: '/api/product/products',
+    productTable: '/api/product/product-table',
     promotionTable: '/api/product/promotion-table',
 };
