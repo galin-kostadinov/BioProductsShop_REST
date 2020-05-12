d2702679cd8a04535b037b2a6d3fea2de6153302
galin-kostadinov <gk_1988@abv.bg>
2020-05-12 20:03:20 +0300

    added editProductConfirm(), deleteProduct(), deleteProductConfirm()

--- app/src/main/java/org/gkk/bioshopapp/web/api/controller/ProductApiController.java
+++ app/src/main/java/org/gkk/bioshopapp/web/api/controller/ProductApiController.java
@@ -1,19 +1,17 @@
 package org.gkk.bioshopapp.web.api.controller;
 
+import org.gkk.bioshopapp.service.model.product.ProductEditServiceModel;
 import org.gkk.bioshopapp.service.service.ProductService;
-import org.gkk.bioshopapp.web.api.model.product.ProductDetailsResponseModel;
-import org.gkk.bioshopapp.web.api.model.product.ProductDiscountTableResponseModel;
-import org.gkk.bioshopapp.web.api.model.product.ProductEditResponseModel;
-import org.gkk.bioshopapp.web.api.model.product.ProductTableResponceModel;
+import org.gkk.bioshopapp.web.api.model.product.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -76,5 +74,29 @@ public class ProductApiController {
                 this.modelMapper.map(this.productService.getProductEditModelById(id), ProductEditResponseModel.class);
 
         return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @PostMapping("/edit/{id}")
+    public void editProductConfirm(@PathVariable String id, ProductEditRequestModel model, HttpServletResponse response) throws IOException {
+        this.productService.editProduct(id, this.modelMapper.map(model, ProductEditServiceModel.class));
+
+        response.sendRedirect("/product/details/" + id);
+    }
+
+    @GetMapping("/delete/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<ProductEditResponseModel> deleteProduct(@PathVariable String id) {
+        ProductEditResponseModel product =
+                this.modelMapper.map(this.productService.getProductEditModelById(id), ProductEditResponseModel.class);
+
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @PostMapping("/delete/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void deleteProductConfirm(@PathVariable String id, HttpServletResponse response) throws IOException {
+        this.productService.deleteProduct(id);
+
+        response.sendRedirect("/product/product-table");
     }
 }
