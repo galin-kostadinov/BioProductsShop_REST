77600aa3e1eb4973861f94703d6e87ca93e9fbbf
galin-kostadinov <gk_1988@abv.bg>
2020-05-12 20:07:09 +0300

    init

---
+++ app/src/main/resources/static/js/product/delete-product.js
@@ -0,0 +1,65 @@
+$(function () {
+    loadProduct();
+});
+
+function loadProduct() {
+    const productId = $('#delete-product').attr('label');
+
+    $.ajax({
+        type: 'GET',
+        url: '/api/product/delete/' + productId,
+        data: 'json',
+        success: function (product) {
+            addProductDOM(product, productId);
+        }
+    })
+}
+
+function addProductDOM({name, description, imgUrl, price}, productId) {
+    $('#delete-product')
+        .append($('<div/>')
+            .addClass('col col-md-4 text-right pr-5')
+            .append($('<img/>', {src: imgUrl})
+                .width('300px')
+                .height('300px')
+            )
+        )
+        .append($('<div/>')
+            .addClass('col col-md-6 text-left pl-5')
+            .append($('<ul/>')
+                .attr('id', 'product-delete-ul')
+                .append($('<li/>')
+                    .append($('<h3/>')
+                        .text(name)
+                    )
+                )
+                .append($('<li/>')
+                    .addClass('text-left pb-3 pt-3')
+                    .append($('<h6/>')
+                        .text(description)
+                    )
+                )
+                .append($('<li/>')
+                    .addClass('text-left pb-3')
+                    .append($('<h5/>')
+                        .text('Price: ' + price + ' $')
+                    )
+                )
+                .append($('<li/>')
+                    .append($('<form/>', {action: '/api/product/delete/' + productId, method: 'POST'})
+                        .append($('<div/>')
+                            .addClass('button-holder d-flex text-left')
+                        )
+                        .append($('<button/>')
+                            .addClass('btn btn-danger font-weight-bold text-white')
+                            .attr('type', 'submit')
+                            .text('Delete')
+                        )
+                    )
+                )
+            )
+        );
+}
+
+
+
