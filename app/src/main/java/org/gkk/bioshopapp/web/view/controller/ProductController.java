32fb63f6c807c16748af11f436111afa94f8395e
galin-kostadinov <gk_1988@abv.bg>
2020-05-13 19:41:29 +0300

    deleted promote(), updated getPromoteForm()

--- app/src/main/java/org/gkk/bioshopapp/web/view/controller/ProductController.java
+++ app/src/main/java/org/gkk/bioshopapp/web/view/controller/ProductController.java
@@ -1,13 +1,11 @@
 package org.gkk.bioshopapp.web.view.controller;
 
 import org.gkk.bioshopapp.service.model.price.PriceDiscountServiceModel;
-import org.gkk.bioshopapp.service.model.product.ProductDetailsServiceModel;
 import org.gkk.bioshopapp.service.service.PriceDiscountService;
 import org.gkk.bioshopapp.service.service.PriceHistoryService;
 import org.gkk.bioshopapp.service.service.ProductService;
 import org.gkk.bioshopapp.web.annotation.PageTitle;
 import org.gkk.bioshopapp.web.view.model.product.PriceDiscountModel;
-import org.gkk.bioshopapp.web.view.model.product.ProductDetailsModel;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -70,7 +68,6 @@ public class ProductController extends BaseController {
     @PageTitle("Edit Product")
     public ModelAndView editProduct(@PathVariable String id, ModelAndView model) {
         model.addObject("productId", id);
-
         return super.view("product/edit-product", model);
     }
 
@@ -86,33 +83,8 @@ public class ProductController extends BaseController {
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PageTitle("Promote Product")
     public ModelAndView getPromoteForm(@PathVariable String id, ModelAndView model) {
-        ProductDetailsServiceModel productServiceModel = this.productService.getProductDetailsModel(id);
-
-        if (productServiceModel.getPromotionalPrice() != null) {
-            return super.redirect("/product/product-table");
-        }
-
-        ProductDetailsModel product =
-                this.modelMapper.map(productServiceModel, ProductDetailsModel.class);
-
-        model.addObject("product", product);
         model.addObject("productId", id);
-        model.addObject("dataTimeNow", LocalDateTime.now().format(formatter));
-
         return super.view("product/promote-form", model);
-    }
-
-    @PostMapping("/promote/{id}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ModelAndView promote(@PathVariable String id, @ModelAttribute PriceDiscountModel model) {
-        PriceDiscountServiceModel priceDiscountServiceModel = this.modelMapper.map(model, PriceDiscountServiceModel.class);
-
-        priceDiscountServiceModel.setFromDate(LocalDateTime.parse(model.getFromDate(), formatter));
-        priceDiscountServiceModel.setToDate(LocalDateTime.parse(model.getToDate(), formatter));
-
-        this.priceHistoryService.setDiscount(id, priceDiscountServiceModel);
-
-        return super.redirect("/product/product-table");
     }
 
     @GetMapping("/promotion-table")
