124d21f14b9d46858c02dec5fab3b29d19aaced8
galin-kostadinov <gk_1988@abv.bg>
2020-05-14 12:55:32 +0300

    deleted

--- app/src/main/resources/static/js/base/urls.js
+++
@@ -1,8 +0,0 @@
-const URLS = {
-    allUsers: '/api/user/all-users',
-    userProfile: '/api/user/profile',
-    orders: '/api/order/orders',
-    products: '/api/product/products',
-    productTable: '/api/product/product-table',
-    promotionTable: '/api/product/promotion-table',
-};
