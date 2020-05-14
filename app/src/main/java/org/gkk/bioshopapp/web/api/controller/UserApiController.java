f59cb9488803fe431034b8f424ebd1ccde260588
galin-kostadinov <gk_1988@abv.bg>
2020-05-14 12:56:20 +0300

    added getEditProfile(), editProfileConfirm(), setAdminRole(), setUserRole()

--- app/src/main/java/org/gkk/bioshopapp/web/api/controller/UserApiController.java
+++ app/src/main/java/org/gkk/bioshopapp/web/api/controller/UserApiController.java
@@ -1,18 +1,20 @@
 package org.gkk.bioshopapp.web.api.controller;
 
+import org.gkk.bioshopapp.service.model.user.UserEditProfileServiceModel;
 import org.gkk.bioshopapp.service.model.user.UserProfileServiceModel;
 import org.gkk.bioshopapp.service.service.UserService;
+import org.gkk.bioshopapp.web.api.model.user.UserEditProfileRequestModel;
 import org.gkk.bioshopapp.web.api.model.user.UserProfileResponseModel;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -47,5 +49,42 @@ public class UserApiController {
         UserProfileResponseModel user = this.modelMapper.map(serviceModel, UserProfileResponseModel.class);
 
         return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/profile/edit")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<UserProfileResponseModel> getEditProfile(HttpSession session) throws Exception {
+        String username = session.getAttribute("username").toString();
+        UserProfileServiceModel serviceModel = this.userService.getUserByUsername(username);
+        UserProfileResponseModel user = this.modelMapper.map(serviceModel, UserProfileResponseModel.class);
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PostMapping("/profile/edit")
+    @PreAuthorize("isAuthenticated()")
+    public void editProfileConfirm(UserEditProfileRequestModel model, HttpServletResponse response) throws IOException {
+        UserEditProfileServiceModel serviceModel = this.modelMapper.map(model, UserEditProfileServiceModel.class);
+
+        try {
+            this.userService.editUserProfile(serviceModel);
+            response.sendRedirect("/user/profile");
+        } catch (Exception e) {
+            response.sendRedirect("/user/profile/edit");
+        }
+    }
+
+    @PostMapping("/set-admin/{id}")
+    @PreAuthorize("hasRole('ROLE_ROOT')")
+    public void setAdminRole(@PathVariable String id, HttpServletResponse response) throws IOException {
+        this.userService.makeAdmin(id);
+        response.sendRedirect("/user/all-users");
+    }
+
+    @PostMapping("/set-user/{id}")
+    @PreAuthorize("hasRole('ROLE_ROOT')")
+    public void setUserRole(@PathVariable String id, HttpServletResponse response) throws IOException {
+        this.userService.makeUser(id);
+        response.sendRedirect("/user/all-users");
     }
 }
