b666599593845d2cdcdcb9bf885ded6b8ac2f755
galin-kostadinov <gk_1988@abv.bg>
2020-05-14 13:00:05 +0300

    updated getEditProfile(), getProfile(); deleted setAdminRole(), setUserRole(), editProfileConfirm()

--- app/src/main/java/org/gkk/bioshopapp/web/view/controller/UserController.java
+++ app/src/main/java/org/gkk/bioshopapp/web/view/controller/UserController.java
@@ -1,85 +1,34 @@
 package org.gkk.bioshopapp.web.view.controller;
 
-import org.gkk.bioshopapp.service.model.user.UserEditProfileServiceModel;
-import org.gkk.bioshopapp.service.model.user.UserProfileServiceModel;
-import org.gkk.bioshopapp.service.service.UserService;
 import org.gkk.bioshopapp.web.annotation.PageTitle;
-import org.gkk.bioshopapp.web.view.model.user.UserEditProfileModel;
-import org.gkk.bioshopapp.web.view.model.user.UserProfileViewModel;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpSession;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
 public class UserController extends BaseController {
-    private final UserService userService;
-    private final ModelMapper modelMapper;
-
-    @Autowired
-    public UserController(UserService userService, ModelMapper modelMapper) {
-        this.userService = userService;
-        this.modelMapper = modelMapper;
-    }
 
     @GetMapping("/profile")
     @PreAuthorize("isAuthenticated()")
     @PageTitle("Profile")
-    public ModelAndView getProfile(ModelAndView model) {
-        return super.view("user/profile", model);
+    public ModelAndView getProfile() {
+        return super.view("user/profile");
     }
 
     @GetMapping("profile/edit")
     @PreAuthorize("isAuthenticated()")
     @PageTitle("Edit Profile")
-    public ModelAndView getEditProfile(HttpSession session, ModelAndView model) throws Exception {
-        String username = session.getAttribute("username").toString();
-        UserProfileServiceModel serviceModel = this.userService.getUserByUsername(username);
-        model.addObject("model", this.modelMapper.map(serviceModel, UserProfileViewModel.class));
-
-        return super.view("user/edit-profile", model);
-    }
-
-    @PostMapping("profile/edit")
-    @PreAuthorize("isAuthenticated()")
-    public ModelAndView editProfileConfirm(@ModelAttribute UserEditProfileModel model) {
-        UserEditProfileServiceModel serviceModel = this.modelMapper.map(model, UserEditProfileServiceModel.class);
-
-        try {
-            this.userService.editUserProfile(serviceModel);
-            return super.redirect("/user/profile");
-        } catch (Exception e) {
-            return super.redirect("/user/profile/edit");
-        }
+    public ModelAndView getEditProfile() {
+        return super.view("user/edit-profile");
     }
 
     @GetMapping("/all-users")
     @PreAuthorize("hasRole('ROLE_ROOT')")
     @PageTitle("Users")
-    public ModelAndView getAllUsers(ModelAndView model) {
-        return super.view("user/all-users", model);
-    }
-
-    @PostMapping("/set-admin/{id}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ModelAndView setAdminRole(@PathVariable String id) {
-        this.userService.makeAdmin(id);
-
-        return super.redirect("/user/all-users");
-    }
-
-    @PostMapping("/set-user/{id}")
-    @PreAuthorize("hasRole('ROLE_ROOT')")
-    public ModelAndView setUserRole(@PathVariable String id) {
-        this.userService.makeUser(id);
-
-        return super.redirect("/user/all-users");
+    public ModelAndView getAllUsers() {
+        return super.view("user/all-users");
     }
 }
