09d2dbb907bafe8a15440a75e9595b8c7f0f523d
galin-kostadinov <gk_1988@abv.bg>
2020-05-16 17:59:02 +0300

    updated imports

--- app/src/main/java/org/gkk/bioshopapp/error/PriceHistoryNotFoundException.java
+++ app/src/main/java/org/gkk/bioshopapp/error/PriceHistoryNotFoundException.java
@@ -1,21 +1,20 @@
 package org.gkk.bioshopapp.error;
 
-import org.gkk.bioshopapp.constant.ErrorMessageConstant;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import static org.gkk.bioshopapp.constant.ErrorMessageConstant.*;
+import static org.gkk.bioshopapp.constant.ErrorMessageConstant.PRICE_HISTORY_NOT_FOUND;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = PRICE_HISTORY_NOT_FOUND)
-public class PriceHishoryNotFoundException extends RuntimeException {
+public class PriceHistoryNotFoundException extends RuntimeException {
 
     private int status;
 
-    public PriceHishoryNotFoundException() {
+    public PriceHistoryNotFoundException() {
         this.status = 404;
     }
 
-    public PriceHishoryNotFoundException(String message) {
+    public PriceHistoryNotFoundException(String message) {
         super(message);
         this.status = 404;
     }
